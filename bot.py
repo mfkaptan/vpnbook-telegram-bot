7d7c00615c330b6e78c0f3b1dddb13e9de5d5f9d
mfkaptan <kaptanmf@gmail.com>
2016-05-08 18:33:03 +0200

    Update according to new telepot functions

--- bot.py
+++ bot.py
@@ -8,44 +8,57 @@ SERVERS = ['euro217.vpnbook.com', 'euro214.vpnbook.com',
            'us1.vpnbook.com', 'us2.vpnbook.com', 'ca1.vpnbook.com',
            'de233.vpnbook.com']
 
-def handle(msg):
-    try:
-    	text = msg['text']
-    	chat_id = msg['chat']['id']
-    except:
-        return
 
-    if text == '/start':
-        show_keyboard = {'keyboard': [['/server', '/password']]}
-        bot.sendMessage(chat_id, 'Server or Password? http://www.vpnbook.com/#pptpvpn', reply_markup=show_keyboard)
+class VPNBoot(telepot.Bot):
+    def __init__(self, *args, **kwargs):
+        super(VPNBoot, self).__init__(*args, **kwargs)
 
-    elif text == '/server':
-        for server in SERVERS:
-            bot.sendMessage(chat_id, server)
+    def handle(self, msg):
+        flavor = telepot.flavor(msg)
+        if flavor != "chat":
+            return
 
-    elif text == '/password':
-        f = urllib.urlopen("http://www.vpnbook.com")
-        b = f.read()
-        f.close()
-        pwd = find_between(b, "Password: ", "</strong>")
-        bot.sendMessage(chat_id, pwd)
+        text, chat_id = None, None
 
+        try:
+            content_type, chat_type, chat_id = telepot.glance(msg)
+            text = msg['text']
+        except:
+            return
+        else:
+            if content_type != "text":
+                return
 
-def find_between(s, first, last):
-    try:
-        start = s.index(first) + len(first)
-        end = s.index(last, start)
-        return s[start:end]
-    except ValueError:
-        return ""
+        if text == '/start':
+            show_keyboard = {'keyboard': [['/server', '/password']]}
+            bot.sendMessage(chat_id, 'Server or Password? http://www.vpnbook.com/#pptpvpn', reply_markup=show_keyboard)
+
+        elif text == '/server':
+            for server in SERVERS:
+                bot.sendMessage(chat_id, server)
+
+        elif text == '/password':
+            f = urllib.urlopen("http://www.vpnbook.com")
+            b = f.read()
+            f.close()
+            pwd = self.find_between(b, "Password: ", "</strong>")
+            bot.sendMessage(chat_id, pwd)
+
+    def find_between(self, s, first, last):
+        try:
+            start = s.index(first) + len(first)
+            end = s.index(last, start)
+            return s[start:end]
+        except ValueError:
+            return ""
 
 
 # Getting the token from command-line is better than embedding it in code,
 # because tokens are supposed to be kept secret.
 TOKEN = sys.argv[1]
 
-bot = telepot.Bot(TOKEN)
-bot.notifyOnMessage(handle)
+bot = VPNBoot(TOKEN)
+bot.message_loop()
 
 print 'Listening ...'
 
